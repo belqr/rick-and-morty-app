dde6a79fa5102b79a5588ff216f9554e5f4b3550
Isabel Queiroz <belqrdev@outlook.com>
2024-07-01 13:57:41 -0300

    config tailwind

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,20 +1,46 @@
 import type { Config } from "tailwindcss";
 
 const config: Config = {
-  content: [
-    "./pages/**/*.{js,ts,jsx,tsx,mdx}",
-    "./components/**/*.{js,ts,jsx,tsx,mdx}",
-    "./app/**/*.{js,ts,jsx,tsx,mdx}",
-  ],
-  theme: {
-    extend: {
-      backgroundImage: {
-        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
-        "gradient-conic":
-          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
-      },
-    },
-  },
-  plugins: [],
+	content: [
+		"./pages/**/*.{js,ts,jsx,tsx,mdx}",
+		"./components/**/*.{js,ts,jsx,tsx,mdx}",
+		"./app/**/*.{js,ts,jsx,tsx,mdx}",
+	],
+	theme: {
+		extend: {
+			colors: {
+				deepBlue: "#12153B",
+				neonGreen: "#39ff14",
+			},
+			backgroundImage: {
+				portal: "url('/portal.png')",
+				fundo: "url('/bg.jpg')",
+			},
+			backgroundSize: {
+				auto: "auto",
+				cover: "cover",
+				contain: "contain",
+				"50%": "50%",
+				"25%": "25%",
+			},
+			keyframes: {
+				neonPulse: {
+					"0%, 100%": {
+						"box-shadow":
+							"0 0 2.5px #39ff14, 0 0 5px #39ff14, 0 0 10px #39ff14, 0 0 10px #39ff14",
+					},
+					"50%": {
+						"box-shadow":
+							"0 0 10px #39ff14, 0 0 10px #39ff14, 0 0 15px #39ff14, 0 0 20px #39ff14",
+					},
+				},
+			},
+			animation: {
+				neonPulse: "neonPulse 2s cubic-bezier(0.4, 0, 0.6, 1) infinite reverse",
+				"spin-slow": "spin 3s linear infinite",
+			},
+		},
+	},
+	plugins: [],
 };
 export default config;
